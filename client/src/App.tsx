3285f9f00e73ff858f7dbc696e2679845837a5a1
Majhi, Prmaod <pramod.majhi@blueshieldca.com>
2018-09-14 12:24:51 -0700

    Intial commit - wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
+import TextField from '@material-ui/core/TextField'
 import * as React from 'react'
 import TimeAgo from 'react-timeago'
 import * as io from 'socket.io-client'
 import { Meas } from './components/Measurement'
 import { Sleep } from './components/Sleep'
 import conf from './conf'
-import page from './img/page.png'
+
+// import page from './img/page.png'
+import footer from './img/footer.png'
+import manifestlogo from './img/manifestmedex.png'
+import nav from './img/nav.png'
+import peak from './img/peak.png'
+import resting from './img/resting.png'
+import sec1 from './img/sec1.png'
+import sleephrs from './img/sleep.png'
+import steps from './img/steps.png'
+import vitalIcon from './img/vitalIcon.png'
+
 import { IData, IMeasurement } from './models/data'
+
 
 import './App.css'
 
@@ -24,7 +37,7 @@ const initialState = {
   sleepMinutes: 0,
   started: false,
   stepsCount: 0,
-  weight: 0,  
+  weight: 0,
 }
 
 type State = Readonly<typeof initialState>
@@ -42,7 +55,7 @@ class App extends React.Component<object, State> {
     socket.on("update", (data: IData) => {
       this.load(data)
     })
-    this.setState({started: true})
+    this.setState({ started: true })
   }
 
   // When the browser connects to the webSocket, the server sends an update message 
@@ -62,21 +75,21 @@ class App extends React.Component<object, State> {
     const response = await fetch(`http://${conf.host}/values`)
     if (response.ok) {
       const data = await response.json()
-      this.load(data)     
+      this.load(data)
     }
   }
 
   public load(data: IData) {
-    if (! data) {
+    if (!data) {
       return
     }
 
-    const {heightFeet, heightInches} = this.convertHeight(data.Height)
+    const { heightFeet, heightInches } = this.convertHeight(data.Height)
     const weight = this.convertWeight(data.Weight)
     const bmi = this.calcBMI(data.Weight, data.Height)
-    const {sleepHours, sleepMinutes} = this.parseSleep(data.SleepHours)
+    const { sleepHours, sleepMinutes } = this.parseSleep(data.SleepHours)
 
-    const newState : any = {
+    const newState: any = {
       bmi,
       heartRate: data.HeartRate && data.HeartRate.value ? Number(data.HeartRate.value) : 0,
       heartRateSource: data.HeartRate && data.HeartRate.source ? data.HeartRate.source : '',
@@ -92,7 +105,7 @@ class App extends React.Component<object, State> {
     }
 
     this.setState(state => {
-      newState.changeSet = this.findChanges(state, newState)      
+      newState.changeSet = this.findChanges(state, newState)
       newState.lastRecorded = new Date()
       return newState
     })
@@ -100,11 +113,11 @@ class App extends React.Component<object, State> {
     // clear the changeset, so 'flash' class is removed from all fields,
     // so they can be flashed next time the change.
     setTimeout(_ => {
-      this.setState({changeSet: new Set()})
+      this.setState({ changeSet: new Set() })
     }, 2000);
   }
 
-  public convertHeight(height: IMeasurement) : {heightFeet: number, heightInches: number, captured: string} {
+  public convertHeight(height: IMeasurement): { heightFeet: number, heightInches: number, captured: string } {
     let heightFeet = 0
     let heightInches = 0
     let captured = 'n/a'
@@ -115,7 +128,7 @@ class App extends React.Component<object, State> {
       heightInches = Math.floor(totalInches % 12)
       captured = `${height.value} M`
     }
-    return {heightFeet, heightInches, captured}
+    return { heightFeet, heightInches, captured }
   }
 
   public convertWeight(weight: IMeasurement) {
@@ -123,9 +136,9 @@ class App extends React.Component<object, State> {
     let captured = 'n/a'
     if (weight && weight.value && weight.uom === 'Kg') {
       pounds = Math.floor(Number(weight.value) * 2.20462)
-      captured = `${weight.value} Kg`        
+      captured = `${weight.value} Kg`
     }
-    return {pounds, captured}
+    return { pounds, captured }
   }
 
   public calcBMI(weight: IMeasurement, height: IMeasurement) {
@@ -140,7 +153,7 @@ class App extends React.Component<object, State> {
   // parses "7h 10m"  to {sleepHours: 7, sleepMinutes: 10}
   // parses "7h"      to {sleepHours: 7, sleepMinutes: 0}
   // parses "10m"     to {sleepHours: 0, sleepMinutes: 10}
-  public parseSleep(sleep: IMeasurement) : {sleepHours: number, sleepMinutes: number} {
+  public parseSleep(sleep: IMeasurement): { sleepHours: number, sleepMinutes: number } {
     let sleepHours = 0
     let sleepMinutes = 0
     if (sleep && sleep.value) {
@@ -150,11 +163,11 @@ class App extends React.Component<object, State> {
         sleepMinutes = Number(match[4] || 0)
       }
     }
-    return {sleepHours, sleepMinutes}
+    return { sleepHours, sleepMinutes }
   }
 
   // returns the names of the keys where the value has changed from the previous value
-  public findChanges(state: State, newState: State) : Set<string> {
+  public findChanges(state: State, newState: State): Set<string> {
     const changeSet = new Set()
 
     if (newState) {
@@ -173,19 +186,175 @@ class App extends React.Component<object, State> {
   }
 
   public render() {
+    
     return (
       <div className="App" >
-        <img className="img" src={page} />
-        <div className="lastRecorded">
-          <span>Last recorded:</span>
-          <span>{' '}</span>
-          <TimeAgo date={this.state.lastRecorded} minPeriod={30}/>
+        { /* <img className="img" src={page} /> */}
+        <div className="container">
+          <div className="sec1">
+            <img src={sec1} />
+          </div>
+          <div className="sec2">
+            <img src={nav} />
+            <div className="vitals">
+              <div className="row1">
+                <div className="vitaltxt">
+                  Vitals
+                </div>
+                <div className="lastRecorded">
+                  <span>Last recorded:</span>
+                  <span>{' '}</span>
+                  <TimeAgo date={this.state.lastRecorded} minPeriod={30} />
+                </div>
+                <div className="vitalIcon"><img src={vitalIcon} /> </div>
+              </div>
+              <div className="row2">
+                <div className="vitalsSec">
+                  <div className="Icon"><img src={resting} /></div>
+                  <div className="ActualData">
+                    <div className="txt">
+                      RESTING BPM
+                    </div>
+                    <div className="result">
+                      {"79 BPM"}
+                    </div>
+                  </div>
+                </div>
+                <div className="vitalsSec">
+                  <div className="Icon"><img src={peak} /></div>
+                  <div className="ActualData">
+                    <div className="txt">
+                      PEAK BPM
+                    </div>
+                    <div className="result">
+                      {"79 BPM"}
+                    </div>
+                  </div>
+                </div>
+                <div className="vitalsSec">
+                  <div className="Icon"> <img src={resting} /></div>
+                  <div className="ActualData">
+                    <div className="txt">
+                      HEART RATE VARIABILITY
+                    </div>
+                    <div className="result">
+                      {"79 BPM"}
+                    </div>
+                  </div>
+                </div>
+                <div className="vitalsSec">
+                  <div className="Icon"><img src={steps} /></div>
+                  <div className="ActualData">
+                    <div className="txt">
+                      STEP COUNT
+                    </div>
+                    <div className="result">
+                      {"2000"}
+                    </div>
+                  </div>
+                </div>
+                <div className="vitalsSec">
+                  <div className="Icon"><img src={sleephrs} /></div>
+                  <div className="ActualData">
+                    <div className="txt">
+                      SLEEP DURATION
+                    </div>
+                    <div className="result">
+                      {"7 hrs"}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row3">
+                <div className="Icon"><img src={manifestlogo} /></div>
+              </div>
+              <div className="field">
+                <div className="fieldTxt">HEIGHT</div>
+                <TextField
+                  
+                  label="Read Only"
+                  defaultValue="Height"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+                <div className="fieldSubTxt">ft</div>
+                <TextField                  
+                  label="Read Only"
+                  defaultValue="in"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+                <div className="fieldSubTxt">in</div>
+              </div>
+              <div className="field">
+                <div className="fieldTxt">WEIGHT</div>
+                <TextField                  
+                  label="Read Only"
+                  defaultValue="in"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+                <div className="fieldSubTxt">lbs</div>
+              </div>
+              <div className="field">
+                <div className="fieldTxt">BMI</div>
+              </div>
+              <div className="field">
+                <div className="fieldTxt">BP</div>
+                <TextField                  
+                  label="Read Only"
+                  defaultValue="in"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+                <TextField                  
+                  label="Read Only"
+                  defaultValue="in"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+              </div>
+              <div className="field">
+                <div className="fieldTxt">PULSE</div>
+                <TextField                  
+                  label="Read Only"
+                  defaultValue="in"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+              </div>
+              <div className="field">
+                <div className="fieldTxt">PULSE</div>
+              </div>
+              <div className="field">
+                <div className="fieldTxt">RR</div>
+                <TextField                  
+                  label="RR"
+                  defaultValue={this.state.bmi.toString()}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="sec3">
+            <img src={footer} />
+          </div>
+
         </div>
+
         <Meas name="restingHeartRate" uom="bpm" state={this.state} />
-        <Meas name="peakHeartRate" uom="bpm" state={this.state}/>
+        <Meas name="peakHeartRate" uom="bpm" state={this.state} />
         <Meas name="heartRateVariability" uom="ms" state={this.state} />
-        <Meas name="stepsCount" uom="steps" state={this.state} />        
-        <Sleep state={this.state} />        
+        <Meas name="stepsCount" uom="steps" state={this.state} />
+        <Sleep state={this.state} />
         <Meas name="heightFeet" state={this.state} />
         <Meas name="heightInches" state={this.state} />
         <Meas name="weight" state={this.state} />
